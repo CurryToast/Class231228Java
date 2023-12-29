4b56e740b4e13a16489551166659a99fc81aca12
unknown <gusqjacjswo39@gmail.com>
2023-12-29 12:50:19 +0900

    한글 유니코드

--- partA/src/basic/day2/A09HangulCharTest.java
+++ partA/src/basic/day2/A09HangulCharTest.java
@@ -10,11 +10,13 @@ package basic.day2;
 public class A09HangulCharTest {
     public static void main(String[] args) {
         char start = '나';
-        int codeNa = '\ub098', codeNet = '\ub0ff';
+        int codeNa = start, codeNet = '냿';
 
-        while (start <= '냇') {
+        while (start <= codeNet) {
             System.out.print(start++);
         }
+
+        System.out.println("\n\nstart = " + start);
 
         System.out.println();
         System.out.println((char)codeNa + "~" + (char)codeNet + "까지의 문자의 개수: " + (codeNet - codeNa + 1));
