7184d5c0c6061861dc20a0eb683c3058127e3086
CurryToast <gusqjacjswo39@gmail.com>
2024-01-02 17:54:30 +0900

    숙제 파일

---
+++ partA/src/basic/day3/HomeCharType.java
@@ -0,0 +1,12 @@
+package basic.day3;
+
+public class HomeCharType {
+    public static void main(String[] args) {
+        String message = "Hello Java17.0~ !?";
+        int upperCount = 0, lowerCount = 0, numberCount = 0, symbolCount = 0;
+
+        for (int i = 0; i < message.length(); i++) {
+            char item = message.charAt(i);
+        }
+    }
+}
