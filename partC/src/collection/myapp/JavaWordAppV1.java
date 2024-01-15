97c269e13da298afd6e587639dfa10072de223ed
CurryToast <gusqjacjswo39@gmail.com>
2024-01-15 11:35:39 +0900

    Map 관련 코드 수정 및 주석 추가

--- partC/src/collection/myapp/JavaWordAppV1.java
+++ partC/src/collection/myapp/JavaWordAppV1.java
@@ -1,11 +1,8 @@
 package collection.myapp;
 
-import java.util.Scanner;
-
 // 단어장 시작 프로그램
 public class JavaWordAppV1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String menu;
 
         // 1. 단어장 생성
@@ -23,63 +20,44 @@ public class JavaWordAppV1 {
         while (true) {
             System.out.println("1. 단어장 전체 출력  2. 단어 조회  3. 단어 삭제");
             System.out.print("메뉴 입력 (end 입력하면 종료, end 이외에 문자열 입력하면 에러 발생함) >>> ");
-            menu = sc.nextLine();
+            menu = System.console().readLine();
             if (menu.equals("end")) {
                 break;
-            } else if (Integer.parseInt(menu) == 1) { // 단어장 전체 출력
-                myBook.wordAllPrint(null);
-            } else if (Integer.parseInt(menu) == 2) { // 단어 조회
-                // 3. 단어 조회
-                String submenu;
-                while (true) {
-                    System.out.print("word or level (end 입력 시 종료) >>> ");
-                    submenu = sc.nextLine();
-                    if (submenu.equals("end")) {
-                        break;
-                    }
+            }
 
+            switch (Integer.parseInt(menu)) {
+                case 1:
+                    myBook.wordAllPrint();
+                    break;
+                case 2:
+                    String submenu;
+                    System.out.print("word or level >>> ");
+                    submenu = System.console().readLine();
                     if (submenu.equals("word")) { // 영어단어 검색
                         String keyword;
-                        while (true) {
-                            System.out.print("검색할 단어 입력 (end 입력 시 종료) >>> ");
-                            keyword = sc.nextLine();
-                            if (keyword.equals("end")) {
-                                break;
-                            }
-
-                            System.out.println(keyword + " 검색 결과 : " + myBook.searchWord(keyword));
-                        }
+                        System.out.print("검색할 단어 입력 (end 입력 시 종료) >>> ");
+                        keyword = System.console().readLine();
+                        System.out.println(keyword + " 검색 결과 : " + myBook.searchWord(keyword));
                     } else if (submenu.equals("level")) { // 레벨 기준 검색
                         int level;
                         System.out.print("검색할 레벨 입력 >>> ");
-                        level = sc.nextInt();
-                        sc.nextLine();
-                        myBook.wordAllPrint(myBook.searchWordByLevel(level));
+                        level = Integer.parseInt(System.console().readLine());
+                        JavaWordBook.wordAllPrint(myBook.searchWordByLevel(level));
                     } else {
                         System.out.println("없는 메뉴 입니다.");
                     }
-
-                }
-            } else if (Integer.parseInt(menu) == 3) { // 단어 삭제
-                // 4. 단어 삭제
-                String remover;
-                while (true) {
-                    System.out.print("삭제할 단어을 입력하시오 >>> (end 입력하면 종료) ");
-                    remover = sc.nextLine();
-                    if (remover.equals("end")) {
-                        break;
-                    }
-
+                    break;
+                case 3:
+                    String remover;
+                    System.out.print("삭제할 단어을 입력하시오 >>> ");
+                    remover = System.console().readLine();
                     myBook.removeWord(remover);
-                    myBook.wordAllPrint(null);
-                }
-            } else {
-                System.out.println("없는 메뉴 입니다!");
+                    myBook.wordAllPrint();
+                    break;
+                default:
+                    System.out.println("없는 메뉴 입니다!");
+                    break;
             }
-
         }
-
-
-        sc.close();
     }
 }
--- partC/src/collection/myapp/JavaWordBook.java
+++ partC/src/collection/myapp/JavaWordBook.java
@@ -8,7 +8,7 @@ import java.util.TreeMap;
 // JavaWord 여러개를 저장할 클래스
 //              ㄴ 조희 기능에 유리한 Map을 사용
 public class JavaWordBook {
-    // key: 영어
+    // key: 영어, 중복 불가, 순서 없음
     // value: 영어, 한글, 레벨
     private Map<String, JavaWord> wordBook;
 
@@ -28,7 +28,7 @@ public class JavaWordBook {
         this.wordBook.put(word.getEnglish(), word);
     }
 
-    // 단어 조회
+    // 단어 조회 - (비교) List에서는 for 반복해서 찾기
     public JavaWord searchWord(String english) {
         return this.wordBook.get(english);
     }
@@ -44,18 +44,27 @@ public class JavaWordBook {
         return newList;
     }
 
-    // 단어 삭제
+    // 단어 삭제 - (비교) List에서는 for 반복해서 찾은 후 인덱스로 삭제
     public void removeWord(String english) {
         this.wordBook.remove(english);
     }
 
-
-    // 전체 단어 출력하기
-    public void wordAllPrint(List<JavaWord> list) {
+    // (인스턴스 메소드) 전체 단어 출력하기
+    public void wordAllPrint() {
         System.out.println("~".repeat(20) + "~ 단어장 ~" + "~".repeat(20));
         System.out.println(String.format("%-15s %-15s\t %s", "<English>", "<Korean>", "<Level>"));
 
-        for (JavaWord word : (list == null) ? this.wordBook.values() : list) { // list가 null이면 단어장 전체 출력, 아니면 list 전체 출력
+        for (JavaWord word : this.wordBook.values()) { // list가 null이면 단어장 전체 출력, 아니면 list 전체 출력
+            System.out.println(String.format("%-15s %-15s\t %d", word.getEnglish(), word.getKorean(), word.getLevel()));
+        }
+    }
+
+    // (스태틱 메소드) 전달받은 단어 리스트를 출력하기
+    public static void wordAllPrint(List<JavaWord> list) {
+        System.out.println("~".repeat(20) + "~ 단어장 ~" + "~".repeat(20));
+        System.out.println(String.format("%-15s %-15s\t %s", "<English>", "<Korean>", "<Level>"));
+
+        for (JavaWord word : list) { // list가 null이면 단어장 전체 출력, 아니면 list 전체 출력
             System.out.println(String.format("%-15s %-15s\t %d", word.getEnglish(), word.getKorean(), word.getLevel()));
         }
     }
