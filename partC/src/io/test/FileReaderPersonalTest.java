cecb91c9069cf5d5f93be72ffbbafe1b7d9fce49
CurryToast <gusqjacjswo39@gmail.com>
2024-01-16 16:36:57 +0900

    개인코드 수정

--- partC/src/io/test/FileReaderPersonalTest.java
+++ partC/src/io/test/FileReaderPersonalTest.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 
 public class FileReaderPersonalTest {
     public static void main(String[] args) throws IOException {
-        FileReader fr = null;
-        try {
-            fr = new FileReader("C:\\Users\\Administrator\\Downloads\\note.txt");
+        try (
+            FileReader fr = new FileReader("C:\\Users\\Administrator\\Downloads\\note.txt");
+        ) {
             int ch;
             while ((ch = fr.read()) != -1) {
                 System.out.print((char)ch);
             }
         } catch (IOException e) {
             System.out.println("파일 리더 예외 : " + e.getMessage());
-        } finally {
-            fr.close();
         }
     }
 }
