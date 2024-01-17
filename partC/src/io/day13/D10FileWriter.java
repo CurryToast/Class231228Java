957d0f4a658e1951d84f9870f8319ea95450f463
CurryToast <gusqjacjswo39@gmail.com>
2024-01-17 09:54:25 +0900

    throws, close

--- partC/src/io/day13/D10FileWriter.java
+++ partC/src/io/day13/D10FileWriter.java
@@ -16,27 +16,27 @@ public class D10FileWriter {
         String filePath = "단어장2.txt";
         int count = 0;
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
-            // 파일의 끝까지 반복
             for(int i=0;i<10;i++) {
                 // bufferedWriter.append("안녕하세요"+i+"\r\n"); // 기존 파일 뒤에 추가하기 => 단 FileWriter(filePath, true) 시에만
                 bufferedWriter.write("안녕하세요"+i+"\r\n"); // 기존 파일에 덮어쓰기
                 count++;
             }
+
         } catch (Exception e) { }
 
         System.out.println("저장한 라인수 : " + count);
     }
 
-    public static void writeByPrintWriter(){        
+    public static void writeByPrintWriter(){
         String filePath = "단어장3.txt";
         int count = 0;
         File file = new File(filePath);
+
         // print,println 메소드 사용할 수 있음.
         try(PrintWriter pw = new PrintWriter(file,Charset.forName("UTF-8"))) {
-            // 파일의 끝까지 반복
             for(int i=0;i<10;i++) {
                 pw.append("할로~"+i+"\r\n");
-                pw.print("안녕하세요"+i+"\r\n");       
+                pw.print("안녕하세요"+i+"\r\n");
                 count++;
             }
         } catch (Exception e) { }
